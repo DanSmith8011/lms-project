0c91ed792a4e3a4cc66ee6e56b68c6a1c60086a3
Dan Smith <dansmithdan7@aol.co.uk>
2026-04-28 22:04:02 +0100

    feat: add Login component with authentication logic

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,9 +1,20 @@
 import { useState } from 'react'
 
+
+
 function Login(){
     const [password, setPassword] = useState('')
     const [username, setUsername] = useState('')
 
+    const handleLogin = async (e) {
+    e.preventDefualt()
+    try {
+        const response = await axios.post('http://127.0.0.1:8000/api/users/login/', {
+    username,
+    password
+})
+    }
+}
 return (
     <div>
         <label>Username</label>
