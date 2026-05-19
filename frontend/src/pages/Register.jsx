e066fc8f4be6713f0edf583bb35dc70c9fcdad96
DanSmith8011 <dansmithdan7@aol.co.uk>
2026-05-19 19:55:10 +0100

    fix: update Register URL for production

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -10,7 +10,7 @@ function Register() {
     const handleRegister = async (e) => {
         e.preventDefault()
         try {
-            await axios.post('http://127.0.0.1:8000/api/users/register/', {
+            await axios.post('https://lms-backend-d72v.onrender.com/api/users/register/', {
                 username,
                 email,
                 password,
