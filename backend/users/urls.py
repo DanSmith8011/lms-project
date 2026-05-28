be9c0b0b7d02e8726673ec930bf9687dfba05015
DanSmith8011 <dansmithdan7@aol.co.uk>
2026-05-28 19:40:15 +0100

    feat: add admin user management, protected routes and fix RBAC

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RegisterView, CustomTokenObtainPairView, UserListView
-
+from .views import RegisterView, CustomTokenObtainPairView, UserListView, UserDetailView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='login'),
     path('all-users/', UserListView.as_view(), name='user-list'),
+    path('all-users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
 ]
